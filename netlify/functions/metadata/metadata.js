4c0c6d96bc3a169b12ae59fda40f4ec44be76b56
Chris Romero <chris.romero@padillaco.com>
2022-08-14 13:26:00 -0400

    more scraping testing

--- netlify/functions/metadata/metadata.js
+++ netlify/functions/metadata/metadata.js
@@ -1,53 +1,15 @@
-const chromium = require('chrome-aws-lambda')
-const puppeteer = require('puppeteer-core')
+const { gotScraping } = require('got-scraping');
 
-exports.handler = async (event, context) => {
-  let theTitle = null
-  let browser = null
-  console.log('spawning chrome headless')
-  try {
-    const executablePath = process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath
+exports.handler = async function (event, context) {
 
-    // setup
-    browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath: executablePath,
-      headless: chromium.headless,
-    })
-
-    // Do stuff with headless chrome
-    const page = await browser.newPage()
-    const targetUrl = 'https://davidwells.io'
-
-    // Goto page and then do stuff
-    await page.goto(targetUrl, {
-      waitUntil: ['domcontentloaded', 'networkidle0']
-    })
-
-    await page.waitForSelector('body')
-
-    theTitle = await page.title()
-
-    console.log('done on page', theTitle)
-  } catch (error) {
-    console.log('error', error)
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: error
-      })
-    }
-  } finally {
-    // close browser
-    if (browser !== null) {
-      await browser.close()
-    }
-  }
+  const { body } = await gotScraping({ url: 'https://www.example.com' });
+  console.log(body);
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      title: theTitle,
+      body: body
     })
   }
-}
+
+};
