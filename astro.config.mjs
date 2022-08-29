bef54185628694b4064eb41eadca2e3cc2697e82
Chris Romero <chris.romero@padillaco.com>
2022-08-29 14:26:17 -0400

    adds netlify adapter for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import lit from "@astrojs/lit";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [lit()]
 });
