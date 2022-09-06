e77482d3e43dd52ed9b638da7415b2441f8b83ae
Chris Romero <chris.romero@padillaco.com>
2022-09-06 17:16:29 -0400

    function updates

--- netlify/functions/metadata/metadata.js
+++ netlify/functions/metadata/metadata.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 exports.handler = async function (event, context) {
   let url = event.queryStringParameters.url;
 
-  let image = null;
+  let meta_image = null;
+  let main_image = null;
   let browser = null;
   
   try {
@@ -14,15 +15,13 @@ exports.handler = async function (event, context) {
     const page = await context.newPage();
     await page.goto(url);
 
-    image = await page.$eval('head', b => {
+    meta_image = await page.$eval('head', b => {
       return b.querySelector('meta[property="og:image"]').getAttribute('content');
     });
 
-    if(!image) {
-      image = await page.$eval('body', b => {
-        return b.querySelector('main img').getAttribute('src');
-      });
-    }
+    main_image = await page.$eval('body', b => {
+      return b.querySelector('main img').getAttribute('src');
+    });
    
   } catch (error) {
     throw error;
@@ -35,7 +34,8 @@ exports.handler = async function (event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      image: image
+      meta_image: meta_image,
+      main_image: main_image
     })
   };
 };
