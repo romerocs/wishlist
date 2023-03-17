9902b112f7e4c182c017bda22d2a28f285f42000
Chris Romero <chris.romero@padillaco.com>
2023-03-16 22:23:17 -0400

    debugging

--- netlify/functions/data.js
+++ netlify/functions/data.js
@@ -23,12 +23,12 @@ exports.handler = async event => {
   // // Did it work?
   // console.log(data, error);
 
-let { data, error } = await supabase
+let { data: list, error } = await supabase
 .from('list')
 .select('*');
 
-console.log(data);
+console.log(list);
 
-return data;
+return list;
 
 }
