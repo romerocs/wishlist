ad11abdd4128feaf795f384a59ae40a647a254db
Chris Romero <chris.romero@padillaco.com>
2023-03-16 21:22:52 -0400

    debugging

--- netlify/functions/data.js
+++ netlify/functions/data.js
@@ -13,14 +13,21 @@ const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
 // Our standard serverless handler function
 exports.handler = async event => {
 
-  // Insert a row
-    const { data, error } = await supabase
-        .from('list')
-        .insert([
-            { note: 'Chris Suprise & Delight (serverless function)' },
-        ]);
+  // // Insert a row
+  //   const { data, error } = await supabase
+  //       .from('list')
+  //       .insert([
+  //           { note: 'Chris Suprise & Delight (serverless function)' },
+  //       ]);
 
-  // Did it work?
-  console.log(data, error);
+  // // Did it work?
+  // console.log(data, error);
+
+  // Get everything from the notes table
+let { data: list, error } = await supabase
+.from('list')
+.select('*');
+
+console.log(data);
   
 }
