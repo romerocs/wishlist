39c5bde24adf4ee1a2d51941be6efdafe93c8dda
Chris Romero <chris.romero@padillaco.com>
2023-07-23 13:02:45 -0400

    adds global top bar

---
+++ src/components/AuthWidget.vue
@@ -0,0 +1,98 @@
+<script>
+import supabase from "../utilities/supabase";
+
+export default {
+  props: {
+    url: String
+  },
+  async created() {
+    const { data, error } = await supabase.auth.getSession();
+
+    const { session } = data;
+    const { user } = session;
+
+    this.isLoggedIn = Boolean(session);
+
+    if(this.isLoggedIn) {
+      this.avatarImg = user.user_metadata.avatar_url;
+    }
+  },
+  mounted() {
+    document.addEventListener("click", this.outsideClickHandler.bind(this));
+  },
+  data() {
+    return {
+      isLoggedIn: false,
+      btnDisabled: false,
+      panelVisibility: "none",
+      avatarImg: ""
+    }
+  },
+  methods: {
+    async signInWithGoogle() {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: "google",
+        options: {
+          redirectTo: `${this.url}/`,
+        },
+      });
+    },
+    async logOut() {
+      const { error } = await supabase.auth.signOut();
+    },
+    toggleVisibility() {
+      if(this.panelVisibility === "block") {
+        this.panelVisibility = "none";
+      }
+      else {
+        this.panelVisibility = "block";
+      }
+    },
+    outsideClickHandler(e) {
+      const closePanel = !e.target.closest(".widget") && this.panelVisibility === "block";
+
+      if(closePanel) {
+        this.panelVisibility = "none";
+      }
+    }
+  },
+};
+</script>
+
+<template>
+  <div class="widget">
+    <button class="btn-avatar" aria-label="Login" @click="toggleVisibility">
+      <img :src="avatarImg" alt="">
+    </button>
+
+    <div class="popover" ref="popover">
+    
+      <button @click="logOut" v-if="isLoggedIn">Sign Out</button>
+    <button @click="signInWithGoogle" v-else="isLoggedIn">Sign In</button>
+    </div>
+
+  </div>
+</template>
+
+<style>
+  .widget {
+    position: relative;
+  }
+
+  .popover {
+    display: v-bind(panelVisibility);
+    position: absolute;
+  }
+  
+  .btn-avatar {
+    background-color: gray;
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+    overflow: hidden;
+  }
+
+  .btn-avatar:disabled {
+    pointer-events: none;
+  }
+</style>
--- src/utilities/supabase.js
+++ src/utilities/supabase.js
@@ -1,8 +1,8 @@
-
+import { useStorageAsync } from '@vueuse/core';
 import { createClient } from '@supabase/supabase-js';
 
 const { PUBLIC_DATABASE_URL, PUBLIC_SUPABASE_SERVICE_API_KEY } = import.meta.env;
 
-const supabase = createClient(PUBLIC_DATABASE_URL, PUBLIC_SUPABASE_SERVICE_API_KEY);
+const supabase = createClient(PUBLIC_DATABASE_URL, PUBLIC_SUPABASE_SERVICE_API_KEY, { auth: { persistSession: true }});
 
 export default supabase;
